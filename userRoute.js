7c47c4de138dc4c705175ddb15bd768a087d6603
jaganath128 <89020214+jaganath128@users.noreply.github.com>
2024-06-21 07:06:48 +0530

    Add files via upload

---
+++ userRoute.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express=require('express');
+
+const router=express();
+
+router.use(express.json());
+
+const userController=require('../controllers/userController')
+
+router.post('/send-otp', userController.sendOtp);
+
+module.exports=router;
